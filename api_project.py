063f82a195a94f44bb145fbf03752731e0e11e90
Natalia Peguero <pegueronatalia@gmail.com>
2024-06-19 01:40:03 +0000

    finished formmating the data and fixing bugs when using the API

--- api_project.py
+++ api_project.py
@@ -1,6 +1,7 @@
 import requests
 import googleapiclient.discovery
 import os
+import pprint
 
 
 YOUTUBE_API = os.environ.get('API_KEY')
@@ -18,14 +19,27 @@ def channel_id(name):
 
   response = request.execute()
 
-  return response
+  return response['items'][0]['id']
 
-def top_5_videos(id, results=5):
+def get_5_videos(id, results=5):
   youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=YOUTUBE_API)
 
-  request = youtube.channels().list(
-    
+  request = youtube.search().list(
+    part='snippet',
+    channelId = id,
+    type='video',
+    maxResults=results
   )
+
+  response = request.execute()
+
+  print("Five Videos of " + response['items'][0]['snippet']['channelTitle'] + '\n')
+
+  for item in response['items']:
+    print('Title: ' + item['snippet']['title'])
+    print('Published at: ' + item['snippet']['publishedAt'])
+    print('Link: ' + 'https://www.youtube.com/watch?v=' + item['id']['videoId'] + '\n')
+    
 
 
 
@@ -35,4 +49,5 @@ def top_5_videos(id, results=5):
 if __name__ == '__main__':
   user_input = input("Enter a youtube channel, without spaces ")
   id = channel_id(user_input)
-  top_5_videos(id)
+  if id:
+    get_5_videos(id)
