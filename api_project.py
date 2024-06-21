d91b245a8e1f63001bc0bafc4512bcb16132ff06
Natalia Peguero <pegueronatalia@gmail.com>
2024-06-21 18:35:18 +0000

    fixing styling format for code

--- api_project.py
+++ api_project.py
@@ -8,56 +8,64 @@ YOUTUBE_API = os.environ.get('API_KEY')
 api_service_name = "youtube"
 api_version = "v3"
 
+
 def channel_id(name):
-  
-  youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=YOUTUBE_API)
 
-  request = youtube.channels().list(
-    part='id',
-    forUsername=name
-  )
+    youtube = googleapiclient.discovery.build(
+      api_service_name, api_version, developerKey=YOUTUBE_API
+    )
 
-  response = request.execute()
+    request = youtube.channels().list(
+      part='id',
+      forUsername=name
+    )
 
-  return response['items'][0]['id']
+    response = request.execute()
+
+    return response['items'][0]['id']
+
 
 def get_5_videos(id, results=5):
-  youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=YOUTUBE_API)
 
-  request = youtube.search().list(
-    part='snippet',
-    channelId = id,
-    type='video',
-    maxResults=results
-  )
+    youtube = googleapiclient.discovery.build(
+      api_service_name, api_version, developerKey=YOUTUBE_API
+    )
 
-  response = request.execute()
+    request = youtube.search().list(
+      part='snippet',
+      channelId = id,
+      type='video',
+      maxResults=results
+    )
 
-  videos = []
-  for item in response['items']:
-    data = {
-    'Title': item['snippet']['title'],
-    'Published at': item['snippet']['publishedAt'],
-    'Link': 'https://www.youtube.com/watch?v=' + item['id']['videoId']
-    }
-    videos.append(data)
+    response = request.execute()
 
-  return videos
-    
+    videos = []
+    for item in response['items']:
+      data = {
+      'Title': item['snippet']['title'],
+      'Published at': item['snippet']['publishedAt'],
+      'Link': 'https://www.youtube.com/watch?v=' + item['id']['videoId']
+      }
+      videos.append(data)
+
+    return videos
+
 
 if __name__ == '__main__':
   user_input = input("Enter a youtube channel, without spaces ")
   id = channel_id(user_input)
   if id:
-   
-    videos = get_5_videos(id)
-    videos_df = pd.DataFrame.from_dict(videos)
 
-    engine = db.create_engine('sqlite:///youtube_api.db')
+      videos = get_5_videos(id)
+      videos_df = pd.DataFrame.from_dict(videos)
 
-    videos_df.to_sql('video', con=engine, if_exists='replace', index=False)
+      engine = db.create_engine('sqlite:///youtube_api.db')
+
+      videos_df.to_sql('video', con = engine, if_exists = 'replace',
+        index = False)
 
     with engine.connect() as connection:
-      query_result = connection.execute(db.text("SELECT * FROM video;")).fetchall()
-      print(pd.DataFrame(query_result))
-
+    query_result = connection.execute(db.text("SELECT * FROM video;")
+    ).fetchall()
+    print(pd.DataFrame(query_result))
